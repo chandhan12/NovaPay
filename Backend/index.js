069eff31c619cbf2ab59836d4e10f7182e59b5d8
chandhan <chandhan.g0401@gmail.com>
2024-11-19 17:39:38 +0530

    initialized novapay project

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const mongoose=require("mongoose");
+const dotenv=require("dotenv")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).
+then(() =>{
+    console.log('connected')
+}).catch((error)=>{
+    console.log(error.message)
+})
+
+const app=express();
+
+app.get("/",(req,res) =>{
+    res.json({
+        msg:"hey"
+    })
+})
+
+
+app.listen(3000,() =>{
+console.log("server is started and running on port 3000 ...!")
+})
