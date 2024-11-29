cea35e6fa2311fb8bf152a347879f9bee0ebaa3b
chandhan <chandhan.g0401@gmail.com>
2024-11-29 11:30:32 +0530

    created pages and added routing

---
+++ Frontend/pages/Sendmoney.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sendmoney = () => {
+  return (
+    <div>
+      sendmoney page
+    </div>
+  )
+}
+
+export default Sendmoney
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
 import './index.css'
+import Signup from '../pages/Signup'
+import Dashboard from '../pages/Dashboard'
+import Signin from '../pages/Signin'
+import Sendmoney from '../pages/Sendmoney'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/sendmoney" element={<Sendmoney/>} />
+
+    </Routes>
+    </BrowserRouter>
+    
+    </>
   )
 }
 
